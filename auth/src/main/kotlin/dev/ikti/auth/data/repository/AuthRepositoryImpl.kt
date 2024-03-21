475883c744567cf0b559796e2ecf24be3bcd0b94
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-03-21 08:52:38 +0700

    feat(auth): login exception handling

---
+++ auth/src/main/kotlin/dev/ikti/auth/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package dev.ikti.auth.data.repository
+
+import dev.ikti.auth.data.model.LoginRequest
+import dev.ikti.auth.data.model.LoginResponse
+import dev.ikti.auth.data.remote.AuthService
+import dev.ikti.auth.domain.repository.AuthRepository
+import dev.ikti.auth.presentation.util.AuthException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthRepositoryImpl @Inject constructor(
+    private val authService: AuthService
+) : AuthRepository {
+    override suspend fun login(request: LoginRequest): Flow<LoginResponse> {
+        return flow {
+            try {
+                emit(authService.login(request))
+            } catch (e: HttpException) {
+                when (e.response()?.code()) {
+                    401 -> throw AuthException.PasswordIncorrectException
+                    404 -> throw AuthException.AccountNotFoundException
+                    else -> throw AuthException.FailedToLoginException
+                }
+            }
+        }
+    }
+}
--- auth/src/main/kotlin/dev/ikti/auth/presentation/LoginViewModel.kt
+++ auth/src/main/kotlin/dev/ikti/auth/presentation/LoginViewModel.kt
@@ -3,37 +3,59 @@ package dev.ikti.auth.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.ikti.core.data.UiState
-import dev.ikti.core.data.model.auth.LoginRequest
-import dev.ikti.core.data.model.auth.LoginResponse
-import dev.ikti.core.domain.usecase.remote.auth.LoginUseCase
+import dev.ikti.auth.data.model.LoginRequest
+import dev.ikti.auth.domain.usecase.LoginUseCase
+import dev.ikti.auth.presentation.util.AuthConstant.ERR_ACCOUNT_NOT_FOUND
+import dev.ikti.auth.presentation.util.AuthConstant.ERR_FAILED_TO_LOGIN
+import dev.ikti.auth.presentation.util.AuthConstant.ERR_FAILED_TO_SET_USER_TOKEN
+import dev.ikti.auth.presentation.util.AuthConstant.ERR_PASSWORD_INCORRECT
+import dev.ikti.auth.presentation.util.AuthConstant.ERR_UNKNOWN_ERROR
+import dev.ikti.auth.presentation.util.AuthException
+import dev.ikti.core.domain.usecase.local.preference.SetUserTokenUseCase
+import dev.ikti.core.util.State
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase
+    private val loginUseCase: LoginUseCase,
+    private val setUserTokenUseCase: SetUserTokenUseCase
 ) : ViewModel() {
-    private val _uiStateLogin: MutableStateFlow<UiState<LoginResponse>> =
-        MutableStateFlow(UiState.Loading)
-    val uiStateLogin: StateFlow<UiState<LoginResponse>> = _uiStateLogin
+    private val _stateLogin: MutableStateFlow<State<Unit>> =
+        MutableStateFlow(State.Empty)
+    val stateLogin: StateFlow<State<Unit>> = _stateLogin
 
     fun login(email: String, password: String) {
+        _stateLogin.value = State.Loading
+
         val request = LoginRequest(email, password)
         viewModelScope.launch {
             try {
-                loginUseCase.execute(request)
-                    .catch {
-                        _uiStateLogin.value = UiState.Error(it.message ?: "Unknown error")
+                val response = loginUseCase.execute(request)
+                response.collect { token ->
+                    try {
+                        setUserTokenUseCase.execute(token.token ?: "")
+                    } catch (_: Exception) {
+                        _stateLogin.value = State.Error(ERR_FAILED_TO_SET_USER_TOKEN)
                     }
-                    .collect {
-                        _uiStateLogin.value = UiState.Success(it)
-                    }
+                }
+
+                _stateLogin.value = State.Success(Unit)
             } catch (e: Exception) {
-                _uiStateLogin.value = UiState.Error(e.message ?: "Unknown error")
+                when (e) {
+                    AuthException.AccountNotFoundException -> _stateLogin.value =
+                        State.Error(ERR_ACCOUNT_NOT_FOUND)
+
+                    AuthException.PasswordIncorrectException -> _stateLogin.value =
+                        State.Error(ERR_PASSWORD_INCORRECT)
+
+                    AuthException.FailedToLoginException -> _stateLogin.value =
+                        State.Error(ERR_FAILED_TO_LOGIN)
+
+                    else -> _stateLogin.value = State.Error(ERR_UNKNOWN_ERROR)
+                }
             }
         }
     }
