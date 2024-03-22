406f95775f02d0b02c7d21e1caf9371545725e3f
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-03-22 10:42:39 +0700

    chore(onboarding): remove unnecessary composables

--- onboarding/src/main/kotlin/dev/ikti/onboarding/util/OnboardingException.kt
+++
@@ -1,7 +0,0 @@
-package dev.ikti.onboarding.util
-
-import dev.ikti.onboarding.util.OnboardingConstant.ERR_FAILED_TO_SET_USER
-
-sealed class OnboardingException(message: String) : Exception(message) {
-    object FailedToSetUserException : OnboardingException(ERR_FAILED_TO_SET_USER)
-}
