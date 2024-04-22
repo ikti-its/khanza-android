a7ebd356cc67d8e4fcbd91e3b39bc67bfd64417b
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-23 03:33:54 +0700

    feat(app): implement splash screen with backward compat

--- app/src/main/kotlin/dev/ikti/khanza/presentation/MainApp.kt
+++ app/src/main/kotlin/dev/ikti/khanza/presentation/MainApp.kt
@@ -1,22 +1,15 @@
 package dev.ikti.khanza.presentation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import dev.ikti.khanza.presentation.navigation.NavigationHost
-import dev.ikti.khanza.presentation.navigation.model.ModuleScreen
 
 @Composable
-fun MainApp() {
+fun MainApp(
+    startDestination: String
+) {
     NavigationHost(
         navController = rememberNavController(),
-        startDestination = ModuleScreen.Onboarding.route
+        startDestination = startDestination
     )
-}
-
-@Preview(showBackground = true, device = Devices.PIXEL_7_PRO)
-@Composable
-fun MainAppPreview() {
-    MainApp()
 }
--- app/src/main/kotlin/dev/ikti/khanza/presentation/MainViewModel.kt
+++ app/src/main/kotlin/dev/ikti/khanza/presentation/MainViewModel.kt
@@ -1,12 +1,51 @@
 package dev.ikti.khanza.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.ikti.core.domain.usecase.preference.GetUserTokenUseCase
+import dev.ikti.core.domain.usecase.preference.ObserveIsNewUserUseCase
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-
+    private val getUserTokenUseCase: GetUserTokenUseCase,
+    private val observeIsNewUserUseCase: ObserveIsNewUserUseCase
 ) : ViewModel() {
+    private val _userToken = MutableLiveData<String>()
+    val userToken: LiveData<String> = _userToken
 
+    private val _isNewUser = MutableLiveData<Boolean>()
+    val isNewUser: LiveData<Boolean> = _isNewUser
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        getUserToken(Unit)
+        observeIsNewUser(Unit)
+    }
+
+    private fun getUserToken(state: Unit) {
+        viewModelScope.launch {
+            getUserTokenUseCase.execute(state)
+                .collect { token ->
+                    _userToken.postValue(token)
+                }
+        }
+    }
+
+    private fun observeIsNewUser(state: Unit) {
+        _isLoading.value = true
+        viewModelScope.launch {
+            observeIsNewUserUseCase.execute(state)
+                .collect { isNew ->
+                    _isNewUser.postValue(isNew)
+                    _isLoading.postValue(false)
+                }
+        }
+    }
 }
