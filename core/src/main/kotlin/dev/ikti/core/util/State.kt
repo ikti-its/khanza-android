e3ea0f58526ab4506e839b3f5520d2cf62ad9740
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-24 07:39:08 +0700

    feat(core): update color and refactor `State` class

--- core/src/main/kotlin/dev/ikti/core/util/State.kt
+++
@@ -1,9 +0,0 @@
-package dev.ikti.core.util
-
-sealed class State<out T : Any?> {
-    data class Success<out T : Any?>(val data: T) : State<T>()
-    data class Error(val error: String) : State<Nothing>()
-    object Loading : State<Nothing>()
-
-    object Empty : State<Nothing>()
-}
