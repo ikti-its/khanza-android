d3aec57de5230147c93aa7e1dca48c2db823d468
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-05-08 23:51:30 +0700

    chore(profile): use remember instead of rememberSaveable

--- profile/src/main/kotlin/dev/ikti/profile/presentation/component/organism/ProfileEdit.kt
+++ profile/src/main/kotlin/dev/ikti/profile/presentation/component/organism/ProfileEdit.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -67,12 +67,12 @@ fun ProfileEdit(
         )
     )
 
-    var newEmail by rememberSaveable { mutableStateOf("") }
-    var newPassword by rememberSaveable { mutableStateOf("") }
-    var newAlamat by rememberSaveable { mutableStateOf("") }
-    var isEmailError by rememberSaveable { mutableStateOf(false) }
-    var isPasswordError by rememberSaveable { mutableStateOf(false) }
-    var newAlamatLocation by rememberSaveable { mutableStateOf(LatLng(alamatLat, alamatLon)) }
+    var newEmail by remember { mutableStateOf("") }
+    var newPassword by remember { mutableStateOf("") }
+    var newAlamat by remember { mutableStateOf("") }
+    var isEmailError by remember { mutableStateOf(false) }
+    var isPasswordError by remember { mutableStateOf(false) }
+    var newAlamatLocation by remember { mutableStateOf(LatLng(alamatLat, alamatLon)) }
 
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(newAlamatLocation, 15f)
