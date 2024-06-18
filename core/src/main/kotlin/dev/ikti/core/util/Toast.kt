6e3293b1634923142675bf471bbc9c524f12040f
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-06-18 10:15:18 +0700

    chore(core): showToast util

---
+++ core/src/main/kotlin/dev/ikti/core/util/Toast.kt
@@ -0,0 +1,15 @@
+package dev.ikti.core.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun showToast(
+    context: Context,
+    message: String
+) {
+    Toast.makeText(
+        context,
+        message,
+        Toast.LENGTH_SHORT
+    ).show()
+}
