032b63a506dd9aa6bf6341fbf5e7b892f9f6b844
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-07-05 10:53:26 +0700

    feat(home): handle no internet error

--- home/src/main/kotlin/dev/ikti/home/presentation/HomeContent.kt
+++ home/src/main/kotlin/dev/ikti/home/presentation/HomeContent.kt
@@ -1,9 +1,17 @@
 package dev.ikti.home.presentation
 
+import android.app.Activity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,15 +20,33 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
 import dev.ikti.core.presentation.component.template.MainScaffold
+import dev.ikti.core.presentation.theme.FontGilroy
+import dev.ikti.core.util.NetworkConstant
 import dev.ikti.core.util.UIState
+import dev.ikti.home.R
 import dev.ikti.home.data.model.HomeResponse
 import dev.ikti.home.presentation.component.molecule.HomeFeatureGrid
 import dev.ikti.home.presentation.component.molecule.HomeHeroCard
@@ -32,6 +58,8 @@ fun HomeContent(
     onLogout: () -> Unit,
     navController: NavHostController
 ) {
+    var isInternetDialogHidden by remember { mutableStateOf(true) }
+
     MainScaffold(
         modifier = modifier.navigationBarsPadding(),
         background = Color(0xFF0A2D27)
@@ -74,7 +102,105 @@ fun HomeContent(
         }
     }
 
-    if (stateHome is UIState.Error) {
-        onLogout()
+    when (stateHome) {
+        is UIState.Error -> {
+            when (stateHome.error) {
+                NetworkConstant.ERR_UNKNOWN_HOST -> isInternetDialogHidden = false
+
+                else -> onLogout()
+            }
+        }
+
+        else -> {}
+    }
+
+    AnimatedVisibility(
+        visible = !isInternetDialogHidden,
+        enter = fadeIn(
+            animationSpec = spring(Spring.DampingRatioLowBouncy, Spring.StiffnessLow)
+        ),
+        exit = fadeOut(
+            animationSpec = spring(Spring.DampingRatioLowBouncy, Spring.StiffnessLow)
+        )
+    ) {
+        val onDismiss = { }
+        val activity = (LocalContext.current as? Activity)
+
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFF7F7F7), RoundedCornerShape(12.dp))
+                    .padding(16.dp)
+            ) {
+                Spacer(Modifier.height(24.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(
+                            id = R.drawable.ic_home_sad
+                        ),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
+                }
+                Spacer(Modifier.height(24.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Tidak Ada Internet",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp,
+                            fontFamily = FontGilroy
+                        ),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(Modifier.height(24.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "OMNIA memerlukan koneksi internet Anda",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 14.sp,
+                            fontFamily = FontGilroy
+                        ),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(Modifier.height(36.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    OutlinedButton(
+                        onClick = { activity?.finish() },
+                        modifier = Modifier
+                            .height(48.dp)
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(30.dp),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = Color.Unspecified,
+                            contentColor = Color(0xFF272727)
+                        ),
+                        border = BorderStroke(1.dp, Color(0xFFE5E7EB))
+                    ) {
+                        Text(
+                            text = "Keluar",
+                            style = TextStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                fontFamily = FontGilroy
+                            )
+                        )
+                    }
+                }
+            }
+        }
     }
 }
