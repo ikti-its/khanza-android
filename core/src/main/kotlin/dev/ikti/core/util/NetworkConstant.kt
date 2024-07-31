997fe2e3ed402e580df3c24712b4ffa573bd4d41
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-07-31 13:43:01 +0700

    chore(core): forbidden exception

--- core/src/main/kotlin/dev/ikti/core/util/NetworkConstant.kt
+++ core/src/main/kotlin/dev/ikti/core/util/NetworkConstant.kt
@@ -3,6 +3,7 @@ package dev.ikti.core.util
 object NetworkConstant {
     const val ERR_BAD_REQUEST = "ErrBadRequest"
     const val ERR_UNAUTHORIZED = "ErrUnauthorized"
+    const val ERR_FORBIDDEN = "ErrForbidden"
     const val ERR_NOT_FOUND = "ErrNotFound"
     const val ERR_FILE_UNSUPPORTED = "ErrFileUnsupported"
     const val ERR_UNKNOWN_HOST = "ErrUnknownHost"
